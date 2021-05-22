e763f2010111dc0813f9ec37999f59068df841aa
lehvu95 <lehvu95@gmail.com>
2021-05-22 17:05:13 -0500

    remove comments

--- minesweeper/src/Board.js
+++ minesweeper/src/Board.js
@@ -39,61 +39,6 @@ export const Board = props => {
     yValues.push(y);
   }
 
-  /*
-  setSurrounding(rows, cols, x, y, board) {
-    let updateBoard = board;
-    for (let i = y - 1; i <= y + 1; i++) {
-      for (let j = x - 1; j <= x + 1; j ++){
-        if (i < 0 || i > rows -1  || j < 0 || j > cols - 1 || board[i][j].value === 'X') {
-          continue;
-        } 
-        updateBoard[i][j].value++;
-      }
-    }
-    return updateBoard;
-  }
-  */
-
-  /*const showCell = (mini, board) => {
-    const tempBoard = board;
-    for (let row = 0; row < mini.length; row++) {
-      for (let col = 0; col < mini[row].length; col++) {
-        const {x, y, value} = mini[row][col];
-        if (tempBoard[y][x].status === 'hide') {
-          tempBoard[y][x].status = 'show';
-          if (value === 0) {
-            getMini(x, y, board);
-          }
-        }
-      }
-    }
-    
-    return tempBoard;
-  }
-  
-  const getMini = (x, y, board) => {
-    const miniBoard = [];
-    for (let i = y - 1; i <= y + 1; i++) {
-      const miniRow = [];
-      for (let j = x - 1; j <= x + 1; j ++) {
-        if (i < 0 || i > rows -1  || j < 0 || j > cols - 1 || board[i][j].value === 'X') {
-          continue;
-        }
-        miniRow.push({x: j, y: i, value: board[i][j].value});
-      }
-      miniBoard.push(miniRow);
-    }
-
-    return miniBoard;
-  }
-
-  const fillBlanks = (x, y, board) => {
-    const miniBoard = getMini(x, y, board);
-    let fillBoard = board;
-    fillBoard = showCell(miniBoard, board);
-    return fillBoard;
-  }*/
-
   const showCell = (board, list) => {
     console.log('showCell');
     const tempBoard = board;
